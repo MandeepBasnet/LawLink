001eb927b8d61c08789e9bef519643d433e04caf
MandeepBasnet <mandeep3basnet@gmail.com>
2025-04-30 17:29:44 +0545

    more clean

--- src/main/java/util/PasswordUtil.java
+++ src/main/java/util/PasswordUtil.java
@@ -16,12 +16,4 @@ public class PasswordUtil {
         return BCrypt.checkpw(password, hashedPassword);
     }
 
-    public static void main(String[] args) {
-        String password = "password";
-        String hashedPassword = hashPassword(password);
-        System.out.println(password);
-        System.out.println(hashedPassword);
-        boolean status = PasswordUtil.verifyPassword("password", "$2a$12$Tu40ThZpwoSj444PuLtM..r85s9RpXX/k3v1Ml/QFwo9.sULkolDm");
-        System.out.println(status);
-    }
 }
